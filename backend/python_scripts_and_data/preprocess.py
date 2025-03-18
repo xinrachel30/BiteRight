151e86c9dc140621297c9b957db160025552c252
jsGrovyle <js2863@cornell.edu>
2025-03-18 17:58:19 -0400

    Created a python scripts and data folder

---
+++ backend/python_scripts_and_data/preprocess.py
@@ -0,0 +1,43 @@
+import pickle 
+import os
+
+vocab = []
+
+with open(os.path.join("data", "foodVocab.txt"), "r") as f: 
+  rawTxt = f.read().lower()
+  vocab = rawTxt.split(", ")
+
+vocab = set(vocab)
+
+with open(os.path.join("data", "comment_score_dict.pkl"), "rb") as file:
+    comm_score_dict = pickle.load(file)
+
+foodComm_score_dict = {}
+
+for comm, score in comm_score_dict.items(): 
+  foodComm = ""
+  for keyword in vocab: 
+    if keyword in comm: 
+      foodComm += str(keyword + ", ")
+  if foodComm != "":
+    foodComm_score_dict.update({foodComm: score}) 
+
+print(foodComm_score_dict)
+print(len(foodComm_score_dict))
+
+with open(os.path.join("data", "foodwords_score_dict.txt"), "w", encoding='utf-8', errors='ignore') as f: 
+  f.write(str(foodComm_score_dict))
+
+with open(os.path.join("data", "foodwords_score_dict.pkl"), "wb") as file:
+  pickle.dump(foodComm_score_dict, file)
+
+
+    
+    
+
+   
+
+
+
+
+
---
+++ backend/python_scripts_and_data/scraper.py
@@ -0,0 +1,45 @@
+import praw
+import time
+import pickle
+import os
+
+reddit_read_only = praw.Reddit(client_id="CYzoKYjkZSnH9r5vRUz8fw", #Jordan's client ID
+                    client_secret="Reajfk_GmsByCj_GHsDR-hsKmQ96LQ", # Jordan's client secret
+                    user_agent="Biteright") #Jordan's user agent
+
+thread_urls = []
+thread_urls.append("https://www.reddit.com/r/EatCheapAndHealthy/comments/oo8uyu/what_are_some_stupidly_easy_food_pairings_that_go/")
+thread_urls.append("https://www.reddit.com/r/Cooking/comments/oodevv/what_weird_food_combinations_have_you_been_served/?chainedPosts=t3_oo8uyu")
+
+#Get all comments regardless of nested structure of comment replies
+def getComments(ledger : dict[str,int], comments) -> dict[str,int]: 
+  ''' Processes all comments regardless of nested structure of comment replies. 
+      arg comments: Praw CommentForest
+      Returns: dict[str,int] where the string is guarenteed to be lowercase
+  '''
+  for comm in comments: 
+    ledger.update({str(comm.body).lower(): int(comm.score)})
+    comm.replies.replace_more(limit=None)
+    if comm.replies.list() != []: 
+      ledger = ledger | getComments(ledger, comm.replies)
+  return ledger
+
+comm_score_dict = {}
+
+for thisThread in thread_urls: 
+  print("Loading Thread:\n")
+  threadContent = reddit_read_only.submission(url=thisThread)
+  while True:
+    try:
+        threadContent.comments.replace_more(limit=None)
+        comm_score_dict = comm_score_dict | getComments({}, threadContent.comments)
+        break
+    except Exception:
+        print("Waiting on replace_more...")
+        time.sleep(5)
+
+with open(os.path.join("data", 'comment_score_dict.txt'), 'w', encoding='utf-8', errors='ignore') as f:
+    f.write(str(comm_score_dict))
+
+with open(os.path.join("data" ,"comment_score_dict.pkl"), "wb") as file:
+    pickle.dump(comm_score_dict, file)
