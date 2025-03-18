4e544c79b5102543178a0b688e7ea156d62e4c62
jsGrovyle <js2863@cornell.edu>
2025-03-17 20:51:28 -0400

    Rewrote scraper.py to capture comment and score.

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,49 +1,44 @@
-from bs4 import BeautifulSoup as BS
-import requests
+import praw
+import time
 
-with open("page1.html", "r", encoding="utf-8") as f: 
-  doc = BS(f, "html.parser")
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
 
-tags = doc.find_all("p")
-#print(tags)
-#print(type(tags))
+reddit_read_only = praw.Reddit(client_id="CYzoKYjkZSnH9r5vRUz8fw", #Jordan's client ID
+                    client_secret="Reajfk_GmsByCj_GHsDR-hsKmQ96LQ", # Jordan's client secret
+                    user_agent="Biteright") #Jordan's user agent
 
-myData = []
-startDelim = "<p>"
-endDelim = "</p>"
+thread_urls = []
+thread_urls.append("https://www.reddit.com/r/EatCheapAndHealthy/comments/oo8uyu/what_are_some_stupidly_easy_food_pairings_that_go/")
+thread_urls.append("https://www.reddit.com/r/Cooking/comments/oodevv/what_weird_food_combinations_have_you_been_served/?chainedPosts=t3_oo8uyu")
 
-parseString = str(tags)
-while parseString != "": 
-  keyStr = ""
-  junkStr, _, parseString = parseString.partition(startDelim)
-  keyStr, _, parseString = parseString.partition(endDelim)
-  if(keyStr == ""): 
-    break
-  myData.append(keyStr)
+#Get all comments regardless of nested structure of comment replies
+def getComments(ledger : dict[str,int], comments) -> dict[str,int]: 
+  ''' Processes all comments regardless of nested structure of comment replies. 
+      arg comments: Praw CommentForest
+      Returns: dict[str,int] where the string is guarenteed to be lowercase
+  '''
+  for comm in comments: 
+    ledger.update({str(comm.body).lower(): int(comm.score)})
+    comm.replies.replace_more(limit=None)
+    if comm.replies.list() != []: 
+      ledger = ledger | getComments(ledger, comm.replies)
+  return ledger
 
-tags = doc.find_all("shreddit-comment-action-row")
-#print(tags)
+comm_score_dict = {}
 
-myScores = []
+for thisThread in thread_urls: 
+  print("Loading Thread:\n")
+  threadContent = reddit_read_only.submission(url=thisThread)
+  while True:
+    try:
+        threadContent.comments.replace_more(limit=None)
+        comm_score_dict = comm_score_dict | getComments({}, threadContent.comments)
+        break
+    except Exception:
+        print("Waiting on replace_more...")
+        time.sleep(5)
 
-parseString = str(tags)
-startDelim = "score=\""
-endDelim = "\""
-while parseString != "": 
-  keyStr = ""
-  junkStr, _, parseString = parseString.partition(startDelim)
-  keyStr, _, parseString = parseString.partition(endDelim)
-  if(keyStr == ""): 
-    break
-  myScores.append(keyStr)
-
-myScores = myScores[0:len(myData)]
-
-my_dict_idx = {k: v for k, v in zip(enumerate(myScores), myScores)}
-my_dict = {k: v for k, v in zip(myData, myScores)}
-
-myIdx = 1
-for k , v in my_dict.items(): 
-  print(str(myIdx) + ". " + str(k) + "\t" + "Score: " + str(v) + "\n")
-  myIdx += 1
-
+with open('tempDataset.txt', 'w', encoding='utf-8', errors='ignore') as f:
+    f.write(str(comm_score_dict))
